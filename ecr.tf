5087f9c2527099bc34b6157022b405b4275e7f4f
Laurynas Montvydas <LAMON@danskebank.lt>
2022-07-05 18:05:41 +0300

    init

--- ecr.tf
+++
@@ -1,9 +0,0 @@
-# ecr.tf | Elastic Container Repository
-
-resource "aws_ecr_repository" "aws-ecr" {
-  name = "${var.app_name}-${var.app_environment}-ecr"
-  tags = {
-    Name        = "${var.app_name}-ecr"
-    Environment = var.app_environment
-  }
-}
